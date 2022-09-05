acd0d798bea8e3705acf41d5488a65d4a1ca02f6
Ivan Inclan <ivan.inclan.327b@gmail.com>
2022-09-05 18:28:16 -0500

    gse arreglo el deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://<1vanInclan>.github.io/react-rick-y-morty/",
+  base: "https://1vanInclan.github.io/react-rick-y-morty/",
   plugins: [react()]
 })
